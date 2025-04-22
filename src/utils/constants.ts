b2a2af2623d58e2763c6f7feb38375ff89d3ee18
Victor Oliveira <victor@outfitlabs.com>
2025-04-21 22:17:30 -0300

    Adding missing constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = "https://fakestoreapi.com";
 export const PRODUCTS_REVALIDATE_TIME_SECONDS = 60;
 export const USERS_REVALIDATE_TIME_SECONDS = 60;
+export const PRODUCTS_SKELETON_COUNT = 4;
