f58d96a9286ed6c563e5c457a42c2f76291c89ae
Victor Oliveira <victor@outfitlabs.com>
2025-04-21 21:34:42 -0300

    Add next config to load images from external API

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://fakestoreapi.com/**")],
+  },
 };
 
 export default nextConfig;
